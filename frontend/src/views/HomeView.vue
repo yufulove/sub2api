da02ea11b6c617da69218ecbbb6d3b08f52f4806
wenchao <wenchao@hnzycfc.com>
2026-04-20 21:44:45 +0800

    清理

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -435,7 +435,7 @@ const isHomeContentUrl = computed(() => {
 const isDark = ref(document.documentElement.classList.contains('dark'))
 
 // GitHub URL
-const githubUrl = 'https://github.com/Wei-Shaw/sub2api'
+//const githubUrl = 'https://github.com/Wei-Shaw/sub2api'
 
 // Auth state
 const isAuthenticated = computed(() => authStore.isAuthenticated)
